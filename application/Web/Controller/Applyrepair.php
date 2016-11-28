21fb30731d8b916964dbcc4499ce06dcb37b5797
litao <724062586@qq.com>
2016-11-28 20:43:13 +0800

    修改命名规则

--- application/Web/Controller/Applyrepair.php
+++ application/Web/Controller/Applyrepair.php
@@ -14,12 +14,12 @@ class ApplyRepair
         $this->request=Request::instance();
     }
 
-    public function write_applyrepair()//方法访问路径 http://localhost/web/applyrepair/write_applyrepair
+    public function write_apply_repair()//方法访问路径 http://localhost/web/apply_repair/write_apply_repair
     {
         return $this->view->fetch('WriteApplyrepair');
     }
 
-    public function create_applyrepair()
+    public function create_apply_repair()
     {
         $data=$this->request->param();
         if($id=db('applyrepair')->insertGetId($data)){ //添加并返回新增主键       
@@ -27,7 +27,7 @@ class ApplyRepair
         }
     }
 
-    public function delete_applyrepair()
+    public function delete_apply_repair()
     {       
         $data=$this->request->param();
         if(db('applyrepair')->delete($data['id'])){  // 根据主键删除  // 条件删除：db('applyrepair')->where('id',1)->delete();    
@@ -35,14 +35,14 @@ class ApplyRepair
         }
     }
 
-    public function select_applyrepair()
+    public function select_apply_repair()
     {    
         $data=$this->request->param();
         $db_data=db('applyrepair')->find($data['id']);  // 根据主键查询（ find查询为空返回null,select返回[] ）  // 条件查询：db('applyrepair')->where('id',1)->find();   
         return $this->view->fetch('SuccessSelectApplyrepair',['data'=>$db_data]);//如果使用select ,这里应该['data'=>$db_data[0]]
     }
 
-    public function update_applyrepair()
+    public function update_apply_repair()
     {    
         $data=$this->request->param();
         $db_data=db('applyrepair')->update($data);  // data包含主键更新，没有id请使用 where('id',1)->update() 
