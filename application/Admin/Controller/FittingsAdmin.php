c7af78c18d42eceb7fae580387be1250ea21e965
learnerssl <312785362@qq.com>
2016-12-08 00:16:26 +0800

    配件管理操作

--- application/Admin/Controller/FittingsAdmin.php
+++ application/Admin/Controller/FittingsAdmin.php
@@ -9,6 +9,7 @@ Class FittingsAdmin {
     public function __construct(){
         $this->view=new View;
         $this->request=Request::instance();
+		define("URL_FITTINGS", '/index.php/Admin/Fittings_admin/FittingsIndex');
     }
     /**
 	 * 配件管理功能首页
@@ -18,20 +19,43 @@ Class FittingsAdmin {
         return $this->view->fetch('FittingsIndex',['data'=>$data]);
     }
 	/**
+	 * 配件编辑操作
+	 */
+	public function FittingsEdit(){
+		if($this->request->param()){
+			$param = $this->request->param();
+			$count = count($param);
+			if($count == 1){
+				$id = $param['id'];
+				$data = db("fittings")->where('id',$id)->find();
+				return $this->view->fetch('FittingsEdit',['data' => $data]);
+			}
+			if($count > 1){
+				if(!is_numeric($param['number']) || !is_numeric($param['id'])){
+					return dr_show_return(2,'参数非法！');
+				}
+				$res = db("fittings")->where('id',$param['id'])->update(['number' => $param['number']]);
+				if($res){
+					return dr_show_return(1,'编辑成功！',URL_FITTINGS);
+				}else{
+					return dr_show_return(2,'编辑失败！');
+				}
+			}
+		}	
+	} 
+	/**
 	 * 配件删除操作
 	 */
 	public function FittingsDel(){
-		$param = $this->request->param();
-		$res = db('fittings')->where('id',$param['id'])->delete();
-		$url = '/index.php/Admin/Fittings_admin/FittingsIndex';
+		$post = $this->request->param();
+		if(!is_numeric($post['id'])){
+			return dr_show_return(2, '参数非法！');
+		}
+		$res = db('fittings')->where('id',$post['id'])->delete();
 		if($res){
-			echo "<script>alert('删除成功');";
-			echo "window.location=".$url;
-			echo "</script>";
+				return dr_show_return(1,'删除成功！',URL_FITTINGS);
 		}else{
-			echo "<script>alert('删除失败');";
-			echo "window.location=".$url;
-			echo "</script>";
+				return dr_show_return(2,'删除失败！');
 		}
 	} 
 	/**
@@ -39,7 +63,19 @@ Class FittingsAdmin {
 	 */
 	public function FittingsAdd(){
 		if($this->request->param()){
-			print_r(3);
+			$post = $this->request->param();
+			if(!is_numeric($post['number'])){
+				return dr_show_return(2,'参数非法！');
+			}
+			if(empty($post['fittingsName']) || empty($post['number'])){
+				return dr_show_return(2,'参数不能为空！');
+			}
+			$res = db("fittings")->insertGetId($post);
+			if($res){
+				return dr_show_return(1,'新增成功！',URL_FITTINGS);
+			}else{
+				return dr_show_return(2,'新增失败！');
+			}
 		}else{
 			return $this->view->fetch('FittingsAdd');
 		}
--- application/common.php
+++ application/common.php
@@ -10,3 +10,30 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+/**
+ * 自定义JSON返回数据格式
+ */
+ function dr_show_return($status,$msg,$url='',$data=array()) {
+	if(!is_numeric($status))
+	{
+		$return['status'] = 0;
+		$return['msg']    = '状态码错误！必须为数字！';
+		goto res;
+	}
+	if(empty($msg))
+	{
+		$return['status'] = 0;
+		$return['msg']    = '提示信息不能为空！';
+		goto res;
+	}
+	$return['status'] = $status;
+	$return['msg']    = $msg;
+	if(!empty($url)){
+		$return['url'] = $url;
+	}
+	if(!empty($data)){
+		$return['data'] = $data;
+	}
+    res:	
+	exit(json_encode($return,JSON_UNESCAPED_UNICODE));
+}
