a40831a9e4185897c542822a552c7a8f72c12a25
learnerssl <312785362@qq.com>
2016-12-18 17:51:17 +0800

    注册，忘记密码操作

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -34,7 +34,46 @@ class Index extends Controller
             }
             $this->redirect(Url::build($url,'',false));
         }else{
-            echo "<script>alert('用户名或账号错误');</script>";
+            $url ="'/index/index/index'";
+            echo "<script>alert('用户名或账号错误！');</script>";
+            echo "<script>window.location = ".$url."</script>";
         }
     }
+    /*
+     * 注册新用户操作
+     */
+    public function register(){
+         return $this->view->fetch('register');
+    } 
+    /*
+     * 忘记密码找回操作
+     */
+    public function forgetPassword(){
+        return $this->view->fetch('forgetPassword');
+    } 
+    /*
+     * ajax异步验证用户名是否被注册
+     */
+    public function ajaxCheckUsername(){
+         $data=$this->request->param();
+         $res = db("user")->where('username',$data['username'])->select();
+         if($res){
+             dr_show_return(2,'该用户名已被注册');
+         }else{
+             dr_show_return(1,'该用户可以注册');
+         }
+    } 
+    /*
+     * ajax异步验证登录密码
+     */
+    public function ajaxCheckPassword(){
+         $data=$this->request->param();
+         $rule = '/^[a-zA-Z0-9]{6,12}$/';
+         $res = preg_match($rule,$data['password']);
+         if($res){
+             dr_show_return(1,'密码有效');
+         }else{
+             dr_show_return(2,'密码格式不正确，请输入6-12位数字或字母');
+         }
+    } 
 }
