dc0336a2dc3c9136bb5f8df3dd2f7296fb1cef34
黄传倩 <huangchuanqian@huangchuanqiandeMacBook-Pro.local>
2017-03-24 15:54:04 +0800

    寄回

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -48,6 +48,18 @@ class Index extends CommonBase
         return $this->view->fetch('repair_order',['list'=>$list]);
         
     }
+    //发货
+    public function send_goods() {
+        $uid = $this->user['id'];
+        $data=$this->request->param();
+        if(!empty($data['back_number'])) {
+            db('apply_repair')->where('id',$data['id'])->update(['back_number' => $data['back_number'],'status'=>6]);
+            echo "<script>alert('操作成功');history.go(-1);</script>";
+           
+        }else{
+            echo "<script>alert('操作失败');</script>";
+        }
+    }
     //维修完成
     public function finish_repair() {
         $data=$this->request->param();
@@ -79,8 +91,9 @@ class Index extends CommonBase
              $result = db('fittings')->where('id',$fittings_data[0])->find();//搜索原数据
              $number = $result['number'] - $fittings_data[1];
              db('fittings')->where('id',$fittings_data[0])->update(['number' => $number]);//修改配件库存数量
-             $inser = ['oid' => $data['aid'], 'fid' => $fittings_data[0],'created_at' => date(),'number' => $fittings_data[1]];
-             db('fitting')->insert($insert);
+             $time = date('Y-m-d',time());
+             $insert = ['oid' => $data['aid'], 'fid' => $fittings_data[0],'created_at' => $time,'number' => $fittings_data[1]];
+             db('fitting_log')->insert($insert);
          }
          if(db('apply_repair')->where('id',$data['aid'])->update(['status' => 4])) {
             echo json_encode(array('status'=> 1,'msg'=>'接单成功'));
